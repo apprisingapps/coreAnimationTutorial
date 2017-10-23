a1c224222bb3fe934b995a9c5cd6b8f761d16a60
Ian Hanson <ianhanson@jennys-mbp>
2017-10-22 20:44:31 -0500

    Image size did not adjust to the screen.

--- coreAnimationTutorial/ViewController.swift
+++ coreAnimationTutorial/ViewController.swift
@@ -1,25 +1,55 @@
-//
-//  ViewController.swift
-//  coreAnimationTutorial
-//
-//  Created by Ian Hanson on 10/22/17.
-//  Copyright © 2017 Ian Hanson. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController:  UIViewController
+{
+    override func viewDidLoad ()
+    {
+        super.viewDidLoad ()
+        
+        setup ()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func setup ()
+    {
+        let redLayer = CALayer ()
+        
+        redLayer.frame = CGRect (x: 50, y: 50, width: 50, height: 50)
+        redLayer.backgroundColor = UIColor.red.cgColor
+        redLayer.shadowColor = UIColor.black.cgColor
+        redLayer.shadowOpacity = 0.8
+        redLayer.shadowRadius = 3
+        
+        // Round corners
+        redLayer.cornerRadius = 25
+        
+        // Set border
+        redLayer.borderColor = UIColor.black.cgColor
+        redLayer.borderWidth = 10
+       
+        let imageLayer = CALayer ()
+        let image = UIImage (named:  "butterflysmall.jpg")!
+        
+        imageLayer.contents = image.cgImage
+        imageLayer.frame = CGRect (x:  0, y:  100, width:  image.size.width, height:  image.size.height)
+        imageLayer.contentsGravity = kCAGravityResizeAspect
+        imageLayer.contentsScale = UIScreen.main.scale
+ 
+        // Create a blank animation using the keyPath "cornerRadius", the property we want to animate
+        let animation = CABasicAnimation (keyPath:  "cornerRadius")
+        
+        // Set the starting value
+        animation.fromValue = redLayer.cornerRadius
+        
+        // Set the completion value
+        animation.toValue = 0
+        
+        // How may times should the animation repeat?
+        animation.repeatCount = 1000
+        
+        // Finally, add the animation to the layer
+        redLayer.add (animation, forKey:  "cornerRadius")
+        
+        self.view.layer.addSublayer (imageLayer)
+        self.view.layer.addSublayer (redLayer)
     }
-
-
 }
-
